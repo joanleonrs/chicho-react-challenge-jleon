e6ed3fde3cad38e4c10ff6878910b302d7b4e865
JOSE ANDRES LEON RAMOS <joanleonrs@gmail.com>
2022-06-25 13:00:47 -0500

    test(utils): improve unit tests folder name and fix identation

--- __tests__/commons/gifts.spec.ts
+++
@@ -1,7 +0,0 @@
-import { getGifts } from "../../src/utils"
-
-test('@Test for getGifts()', () => {
-    const expectedData = {"bici": 3, "coche": 2, "pelota": 1, "peluche": 1 }
-    const giftsResult = getGifts()
-    expect(giftsResult).toEqual(expectedData)
-})
---
+++ __tests__/utils/gifts.spec.ts
@@ -0,0 +1,9 @@
+import { getGifts } from '../../src/utils'
+
+test('@Test for getGifts()', () => {
+  const expectedData = {
+    'bici': 3, 'coche': 2, 'pelota': 1, 'peluche': 1
+  }
+  const giftsResult = getGifts()
+  expect(giftsResult).toEqual(expectedData)
+})
